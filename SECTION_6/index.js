e440c465fc041c621e630f6c729e7195635f6b4c
infosabbir <infosabbir9@gmail.com>
2026-05-12 13:28:20 +0600

    Returning json handling response url

--- SECTION_6/index.js
+++ SECTION_6/index.js
@@ -2,18 +2,27 @@ const http = require("http");
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
-    res.setHeader('Content-Type', 'plain/text');
+const products = [{ name: 'banana' }, { name: 'apple' }, { name: 'orange' }];
 
-    if (req.method === 'GET') {
-        res.statusCode = 200;
-        res.end('Hello, This is Nodejs HTTP server - Get request handled!');
+server.on('request', (req, res) => {
+    console.log(req.url);
+    if (req.url === '/products') {
+        if (req.method === 'GET') {
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 200;
+            res.end(JSON.stringify(products));
+        } else {
+            res.setHeader('Content-Type', 'text/plain');
+            res.statusCode = 405;
+            res.end('Method not Allowed');
+        }
     } else {
-        res.statusCode = 405;
-        res.end('Method not Allowed');
+        res.setHeader('Content-Type', 'text/plain');
+        res.statusCode = 404;
+        res.end('Page Not Fount');
     }
 
-})
+});
 
 server.listen(3000, () => {
     console.log("Server is up and running on port 3000");
