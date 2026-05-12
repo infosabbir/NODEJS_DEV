681dca48be7960fe4da1a3d094a5def69df43849
infosabbir <infosabbir9@gmail.com>
2026-05-12 09:16:13 +0600

    Handling GET request

--- SECTION_6/index.js
+++ SECTION_6/index.js
@@ -1,12 +1,19 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    // res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.setHeader('Content-Type', 'text/plain');
-    res.statusCode = 200;
+const server = http.createServer();
 
-    res.end('Hello, This is Nodejs http server!');
-});
+server.on('request', (req, res) => {
+    res.setHeader('Content-Type', 'plain/text');
+
+    if (req.method === 'GET') {
+        res.statusCode = 200;
+        res.end('Hello, This is Nodejs HTTP server - Get request handled!');
+    } else {
+        res.statusCode = 405;
+        res.end('Method not Allowed');
+    }
+
+})
 
 server.listen(3000, () => {
     console.log("Server is up and running on port 3000");
