1c44f87950f2a571df24acdc6a226e47b32890e3
infosabbir <infosabbir9@gmail.com>
2026-05-14 11:53:24 +0600

    Handling route

--- SECTION_7/index.js
+++ SECTION_7/index.js
@@ -2,14 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
+app.get("/", (req, res, next) => {
+    console.log('root route');
+    res.send('<h1>response from root (/) route!!</h1>');
 });
 
-app.use((req, res, next) => {
+app.post("/add-product", (req, res, next) => {
+    console.log(req.body);
+    console.log('first middleware');
+    res.send('<h1>response from the /add-product Route!</h1>');
+});
+
+app.get("/product", (req, res, next) => {
     console.log('Second Middleware');
-    res.send('<h1>response from the second middleware</h1>');
+    res.send('<h1>response from the /product route!</h1>');
 })
 
 app.listen(3000, () => console.log('Listening port on 3000'))
