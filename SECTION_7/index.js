81921d22415f9cc709ad3fd09c537995a5cc5e01
infosabbir <infosabbir9@gmail.com>
2026-05-14 11:25:35 +0600

    understanding how middleware works

--- SECTION_7/index.js
+++ SECTION_7/index.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('Second Middleware');
+    res.send('<h1>response from the second middleware</h1>');
 })
 
 app.listen(3000, () => console.log('Listening port on 3000'))
