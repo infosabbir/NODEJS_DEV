1e3be2a47c79702d296a3ed2e3e5afbcc9965a6c
infosabbir <infosabbir9@gmail.com>
2026-05-14 11:03:38 +0600

    working with middleware

--- SECTION_7/index.js
+++ SECTION_7/index.js
@@ -2,4 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, ()=> console.log('Listening port on 3000'))
+app.use((req, res, next) => {
+    console.log('first middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Second Middleware');
+})
+
+app.listen(3000, () => console.log('Listening port on 3000'))
