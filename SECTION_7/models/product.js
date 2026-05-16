cd8968816f20408c1f9b4e6260b0818dec7fa2ac
infosabbir <infosabbir9@gmail.com>
2026-05-16 07:14:42 +0600

    Creating Product model

---
+++ SECTION_7/models/product.js
@@ -0,0 +1,40 @@
+
+const fs = require('fs');
+
+const path = require('path');
+
+const productDbPath = path.join(__dirname, '../db/product.json');
+
+module.exports = class Product {
+    constructor(
+        name,
+        price,
+
+    ) {
+        this.name = name;
+        this.price = price;
+
+    }
+
+    async save() {
+        fs.readFile(productDbPath, 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            const productData = JSON.parse(data);
+
+            productData.push({ id: productData.length + 1, name: this.name, price: this.price });
+
+            fs.writeFile(productDbPath, JSON.stringify(productData), 'utf-8', (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+
+                }
+                console.log('DATA APPENDED SUCCESSFULLY!');
+            })
+        });
+    }
+}
