5da19cd983032d04b3f7cae2f8ba925cfe3ae34d
infosabbir <infosabbir9@gmail.com>
2026-05-16 15:31:05 +0600

    express js done

--- SECTION_7/controller/product.js
+++ SECTION_7/controller/product.js
@@ -19,3 +19,16 @@ exports.getAllProduct = (req, res,) => {
 
     res.status(200).json({ product });
 }
+
+exports.deleteOne = async (req, res) => {
+    const id = req.params.id;
+    let result;
+    try {
+
+        result = await Product.deleteOne(+id);
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
+
+    res.status(200).json(result);
+}
--- SECTION_7/routes/admin.js
+++ SECTION_7/routes/admin.js
@@ -1,11 +1,12 @@
 
 const express = require("express");
 
-const { addProduct } = require("../controller/product");
+const { addProduct, deleteOne } = require("../controller/product");
 
 const router = express.Router();
 
 router.post("/add-product", addProduct);
 
+router.delete("/delete-product/:id", deleteOne);
 
 module.exports = router;
